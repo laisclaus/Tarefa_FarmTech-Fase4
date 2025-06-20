6c50bd1f0221dd81e5d8f5777aa1e96beff858a9
laisclaus <laisclaus@gmail.com>
2025-06-20 17:40:07 -0300

    Create Data input.py

---
+++ Data input.py
@@ -0,0 +1,31 @@
+!pip install paho-mqtt
+import paho.mqtt.client as mqtt
+from Database.db_actions import inserir_dados
+
+# MQTT settings
+broker = "broker.hivemq.com"
+port = 1883
+topic = "farmtech/fase4/dados"
+
+# Called when connected to broker
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("✅ Connected to MQTT broker.")
+        client.subscribe(topic)
+    else:
+        print(f"❌ Failed to connect. Return code={rc}")
+
+# Called when a message is received
+def on_message(client, userdata, msg):
+    #print(f"\n📥 Topic: {msg.topic}")
+    esp32_readings = msg.payload.decode()
+    inserir_dados(esp32_readings["umidade"], esp32_readings["temperatura"], esp32_readings["ph"], esp32_readings["presenca_fosforo"], esp32_readings["presenca_potassio"], esp32_readings["bomba_ligada"])
+
+# Set up client and callbacks
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+# Connect and listen
+client.connect(broker, port, keepalive=60)
+client.loop_forever()
